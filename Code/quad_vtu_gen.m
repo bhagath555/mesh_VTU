e4afc72ba647364850046584a758105af0ff1546
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2021-12-07 10:58:48 +0530

    Update 2d quad generation script.

--- Code/quad_vtu_gen.m
+++ Code/quad_vtu_gen.m
@@ -1,2 +1,5 @@
 pnts = [0,5,10,0,5,10,0,5,10; 0 0 0  4 4 4 8 8 8; 0 0 0 0 0 0 0 0 0];
-quad_vtu2D(verts, 'Quad.vtu')
+grid = [2,2];
+Pdata = randn(3, size(pnts,2)); % Size is 2, number of points 
+Cdata = randn(1,grid(1)*grid(2)); % Size is 1, number of cells
+quad_vtu2D(pnts, grid, Pdata, Cdata, '../VTU_files/2DQuad.vtu')
