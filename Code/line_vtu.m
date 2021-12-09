93bbef11c17e4a54ab73f4da192cbb19fcff1fd6
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2021-12-09 12:01:25 +0530

    Add line (type=3) VTU exporter function.

---
+++ Code/line_vtu.m
@@ -0,0 +1,18 @@
+function line_vtu(pnts, p_data, c_data, filename)
+
+no_cells = size(pnts,2)-1;
+
+% Calculating Cell connectivity data, (conectivity, offset, and types)
+conct = zeros(2,no_cells);
+offset = 2:2:2*no_cells;
+types = 3*ones(no_cells,1);
+
+% Construct cell connectivity array
+el_cnct = [0,1];
+for i =1:no_cells
+    conct(:,i) = i*el_cnct;
+
+write_VTU(p_data, c_data, pnts, conct, offset, types, filename)
+
+end
+
