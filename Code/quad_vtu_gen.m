6e24a4a13a0ade4baadb97a271685485a7c79a40
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2021-12-09 10:56:33 +0530

    Add description to the quad VTU generating script

--- Code/quad_vtu_gen.m
+++ Code/quad_vtu_gen.m
@@ -1,5 +1,9 @@
+% This scripts generates the VTU file with Quad elements.
+% Simple geometry data and some random dislacement and cell data is
+% cosidered for working and visualization purpose.
 pnts = [0,5,10,0,5,10,0,5,10; 0 0 0  4 4 4 8 8 8; 0 0 0 0 0 0 0 0 0];
 grid = [2,2];
-Pdata = randn(3, size(pnts,2)); % Size is 2, number of points 
-Cdata = randn(1,grid(1)*grid(2)); % Size is 1, number of cells
+Pdata = randn(3, size(pnts,2)); % Point Data (3, number of points)
+Cdata = randn(1,grid(1)*grid(2)); % Cell Data (1, number of cells)
+% The VTU file with above data will be saved in above_directory/VTU_files
 quad_vtu2D(pnts, grid, Pdata, Cdata, '../VTU_files/2DQuad.vtu')
