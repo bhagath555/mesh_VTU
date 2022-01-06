330f2d9642a35990632f412a286aed856c701660
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2022-01-06 22:43:19 +0530

    Add comments and provide updated file naming conventions.

--- Code/examples/gen_line_vtu.m
+++ Code/examples/gen_line_vtu.m
@@ -6,10 +6,12 @@ pnts = [0,1,2,3,4;
         0,0,0,0,0];
 
 cells = size(pnts,2)-1;
-p_data = randn(3, size(pnts,2)); % Point Data (3, number of points)
-c_data = randn(1,cells); % Cell Data (1, number of cells)
+p_data = [0,4,1,5,3;
+          0,4,1,5,3;
+          0,4,1,5,3];% Point Data (3, number of points)
+c_data = [1,0,2,1]; % Cell Data (1, number of cells)
 % The VTU file with above data will be store here.s
-filename = '../VTU_files/Line.vtu';
+filename = '../../VTU_files/Line.vtu';
 % Topology information
 [conct, offset, types] = topo_line(pnts);
 write_VTU(p_data, c_data, pnts, conct, offset, types, filename)
--- Code/examples/gen_quad_vtu.m
+++ Code/examples/gen_quad_vtu.m
@@ -1,13 +1,17 @@
 % This scripts generates the VTU file with Quad elements.
 % Simple geometry data and some random dislacement and cell data is
 % cosidered for working and visualization purpose.
-pnts = [0 5 10 0 5 10 0 5 10; 0 0 0 4 4 4 8 8 8; 0 0 0 0 0 0 0 0 0];
+pnts = [0 5 10 0 5 10 0 5 10;
+        0 0  0 4 4 4  8 8  8;
+        0 0  0 0 0 0  0 0  0];
 gx = 2;
 gy = 2;
+% Provide your own custome point data and cell data to understand ...
+% the assignment of these data sets for visualization.
 p_data = randn(3, size(pnts,2)); % Point Data (3, number of points)
 c_data = randn(1,gx*gy); % Cell Data (1, number of cells)
 % The VTU file with above data will be store here.s
-filename = '../VTU_files/2DQuad.vtu';
+filename = '../../VTU_files/Quad.vtu';
 % Topology information
 [conct, offset, types] = topo_quad(pnts, gx, gy);
 write_VTU(p_data, c_data, pnts, conct, offset, types, filename)
